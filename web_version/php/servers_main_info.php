0f5cbfb359ec4373eed10901fc1363233be27b39
Sergey <sk171299@gmail.com>
2020-05-16 21:35:55 +0300

    delegate funcs for value's status

--- web_version/php/servers_main_info.php
+++ web_version/php/servers_main_info.php
@@ -1,19 +1,31 @@
-<?php if (!empty($_servers_main_info)) {
+<?php
+function get_status_avg_con($avg_con){
+    if ($avg_con > 0 && $avg_con < 5) return "norm";
+    elseif ($avg_con >= 5 && $avg_con < 10) return "warn";
+    else return "error";
+}
+function get_status_avg_upld($avg_upld){
+    if ($avg_upld > 0 && $avg_upld < 2) return "norm";
+    elseif ($avg_upld >= 2 && $avg_upld < 4) return "warn";
+    else return "error";
+}
+function get_status_number_error($number_err){
+    if ($number_err == 0) return "norm";
+    elseif ($number_err >= 1 && $number_err < 10) return "warn";
+    else return "error";
+}
+
+
+if (!empty($_servers_main_info)) {
     foreach ($_servers_main_info as $server) {
         $avg_con = $server["avg_time_connection"];
-        if ($avg_con > 0 && $avg_con < 5) $status_avg_con = "norm";
-        elseif ($avg_con >= 5 && $avg_con < 10) $status_avg_con = "warn";
-        else $status_avg_con = "error";
+        $status_avg_con = get_status_avg_con($avg_con);
 
         $avg_upld = $server["avg_time_upload"];
-        if ($avg_upld > 0 && $avg_upld < 2) $status_avg_upld = "norm";
-        elseif ($avg_upld >= 2 && $avg_upld < 4) $status_avg_upld = "warn";
-        else $status_avg_upld = "error";
+        $status_avg_upld = get_status_avg_upld($avg_upld);
 
         $number_err = $server["number_error"];
-        if ($number_err == 0) $status_number_err = "norm";
-        elseif ($number_err >= 1 && $number_err < 10) $status_number_err = "warn";
-        else $status_number_err = "error";
+        $status_number_err = get_status_number_error($number_err);
 
         $status_date = "error"
     ?>
