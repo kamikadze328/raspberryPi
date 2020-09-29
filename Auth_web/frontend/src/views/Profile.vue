3fc455af1d558c34add227f870ef2e8d982dfee4
Sergey <48965524+kamikadze328@users.noreply.github.com>
2020-09-29 20:56:43 +0300

    move styles

--- Auth_web/frontend/src/views/Profile.vue
+++ Auth_web/frontend/src/views/Profile.vue
@@ -2,22 +2,22 @@
   <div class="profile-container">
     <div class="main-container">
       <section v-show="isAdmin" class="admin-section">
-        <router-link class="href" to="admin">Панель управления</router-link>
+        <router-link class="href red-button my-button" to="admin">Панель управления</router-link>
       </section>
       <section class="user-section">
-        <a class="href" href="/syncdata/">
+        <a class="href my-button" href="/syncdata/">
           <div class="href-text">Syncdata</div>
         </a>
-        <a class="href" href="/graphs/">
+        <a class="href my-button" href="/graphs/">
           <div class="href-text">Графики</div>
         </a>
-        <a class="href" href="/unp/">
+        <a class="href my-button" href="/unp/">
           <div class="href-text">UNP</div>
         </a>
-        <a class="href" href="/unp/info_teg.html">
+        <a class="href my-button" href="/unp/info_teg.html">
           <div class="href-text">Информация об устройствах</div>
         </a>
-        <a class="href" href="/unp/info_dev_err.html">
+        <a class="href my-button" href="/unp/info_dev_err.html">
           <div class="href-text">Ошибки устройств</div>
         </a>
       </section>
@@ -40,8 +40,10 @@ export default {
       return this.$cookies.isKey('isAdmin') ? this.$cookies.get('isAdmin') === 'true' : false
     }
   },
-  beforeRouteEnter (to, from, next) {
-    next(vm => {vm.$mydata.isAuth()})
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.$mydata.isAuth()
+    })
   },
 }
 </script>
@@ -63,29 +65,14 @@ export default {
   height: 100px;
   text-align: center;
   border-radius: 5px;
-  border: 2px solid #348fe2;
   padding: 20px 5px;
   margin: 10px 10px;
-  transition: all .3s cubic-bezier(.6, 0, .4, 1);
   font-size: 1.6rem;
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
-.admin-section .href {
-  border-color: #ff5b57;
-}
-
-.admin-section .href:hover {
-  background: #ff5b57;
-  color: white;
-}
-
-.href:hover {
-  background: #348fe2;
-  color: white;
-}
 
 @media (max-width: 750px) {
   .href {
