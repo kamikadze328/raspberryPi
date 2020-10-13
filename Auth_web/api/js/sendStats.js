972d58be70fda804718b482206a6e8caa66c9f0b
Sergey <48965524+kamikadze328@users.noreply.github.com>
2020-10-13 22:00:31 +0300

    fix not getting response after fast redirect

--- Auth_web/api/js/sendStats.js
+++ Auth_web/api/js/sendStats.js
@@ -1,28 +1,36 @@
-document.addEventListener('mousemove', onActivity)
-document.addEventListener('mousedown', onActivity)
-document.addEventListener('keydown', onActivity)
-document.addEventListener('scroll', onActivity)
-document.addEventListener('touchstart', onActivity)
-
-const currentPath = () => document.location.pathname
+document.addEventListener('mousemove', onActivity);
+document.addEventListener('mousedown', onActivity);
+document.addEventListener('keydown', onActivity);
+document.addEventListener('scroll', onActivity);
+document.addEventListener('touchstart', onActivity);
+window.addEventListener('beforeunload', async () => {
+    while(lastSending.isSending){
+        console.log('here');
+        await sleep(100);
+    }
+});
+const currentPath = () => document.location.pathname;
 let lastSending = {
     date: new Date(),
     path: currentPath(),
-    isSending: false
-}
-sendStats()
-
+    isSending: false,
+};
+sendStats();
 function onActivity(){
-    (!lastSending.isSending && (new Date - lastSending.date > 60000 || lastSending.path !== currentPath())) ? sendStats() : null;
+    if(!lastSending.isSending && (new Date - lastSending.date > 60000 || lastSending.path !== currentPath()))
+        sendStats();
 }
 
 function sendStats(){
-    lastSending.isSending = true
+    lastSending.isSending = true;
     fetch('/api/statistics/stats.php', {
         method: 'POST',
     }).then(() =>{
-        lastSending.date = new Date
-        lastSending.path = document.location.pathname
-        lastSending.isSending = false
+        lastSending.date = new Date;
+        lastSending.path = document.location.pathname;
+        lastSending.isSending = false;
     })
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
