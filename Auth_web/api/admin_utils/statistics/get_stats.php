0a8c4fcce275e487c99288cf0d713f6804e41733
Sergey <48965524+kamikadze328@users.noreply.github.com>
2020-10-13 21:59:45 +0300

    finished permissions API

--- Auth_web/api/admin_utils/statistics/get_stats.php
+++ Auth_web/api/admin_utils/statistics/get_stats.php
@@ -1,10 +1,10 @@
 <?php
 /** @noinspection PhpUndefinedVariableInspection */
-include_once $_SERVER['DOCUMENT_ROOT'] . '/api/objects/DBManager.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/api/config/core.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/api/config/CodesAndMessages.php';
 
 if (isset($user_data['date_min']) && isset($user_data['date_max'])) {
-    $data = $database->get_sessions(get_seconds_from_str_ms($user_data['date_min']), get_seconds_from_str_ms($user_data['date_max']));
+    $data = $db->get_sessions(get_seconds_from_str_ms($user_data['date_min']), get_seconds_from_str_ms($user_data['date_max']));
     if ($data === false) {
         $error_code = CodesAndMessages::DB_ERROR;
     }
