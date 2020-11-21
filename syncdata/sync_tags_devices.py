55dd4dc3558ff4d0d10ad99f3be3863890bec8cf
Sergey <48965524+kamikadze328@users.noreply.github.com>
2020-11-21 11:16:21 +0300

    printing during runtime and fix bugs

--- syncdata/sync_tags_devices.py
+++ syncdata/sync_tags_devices.py
@@ -15,6 +15,8 @@ table_in_db = {
 
 conf_devices_from_file = Logger.read_json_file(Config.CONF_DEVICE_PATH)
 conf_tags_from_file = Logger.read_json_file(Config.CONF_TAG_PATH)
+if conf_devices_from_file is None or conf_tags_from_file is None or len(conf_tags_from_file) == 0 or len(conf_tags_from_file) ==0:
+    raise Exception('device or tag file not exists')
 
 def prepare_devices(devices):
     prepared_devices = list()
@@ -51,15 +53,24 @@ statistics_rows = []
 if configs_servers:
     conf_tags = prepare_tags(conf_tags_from_file)
     conf_devices = prepare_devices(conf_devices_from_file)
-
+    print 'inside config servers'
+    print 'tags - ' + str(len(conf_tags))
+    print 'devices - ' + str(len(conf_devices))
     # noinspection PyBroadException
-    try:
-        for config_server in configs_servers:
+
+    for config_server in configs_servers:
+        try:
+            print 'try - ', config_server.get('host')
             server = DB.Server(**config_server)
             connect_time = server.connect()
             server.delete_by_factory_id_and_replace(conf_devices, table_in_db.get('devices'), Config.FACTORY_ID)
             server.delete_by_factory_id_and_replace(conf_tags, table_in_db.get('tags'), Config.FACTORY_ID)
-
-    except:
-        print sys.exc_info()[1]
-        Logger.write('sync tag_dev - ' +str(sys.exc_info()[1]), Logger.LogType.ERROR)
+            print 'Success?'
+        except:
+            print sys.exc_info()[0]
+            print sys.exc_info()[1]
+            Logger.write('sync tag_dev - ' +str(sys.exc_info()[1]), Logger.LogType.ERROR)
+            raise
+    print 'End'
+else:
+    Logger.write('Server`s config is empty', Logger.LogType.WARN)
